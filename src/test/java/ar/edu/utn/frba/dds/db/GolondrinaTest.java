731f35de5d2b59d31b2b53541f73a4f823b67da2
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-08 09:23:55 -0300

    resulta que no hacía falta especificar las clases * https://stackoverflow.com/questions/4726521/hibernate-jpa-unit-tests-autodection-does-not-work

---
+++ src/test/java/ar/edu/utn/frba/dds/db/GolondrinaTest.java
@@ -0,0 +1,21 @@
+package ar.edu.utn.frba.dds.db;
+
+import ar.edu.utn.frba.dds.pepita.Golondrina;
+import org.junit.jupiter.api.Test;
+import org.uqbarproject.jpa.java8.extras.WithGlobalEntityManager;
+import org.uqbarproject.jpa.java8.extras.test.AbstractPersistenceTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GolondrinaTest extends AbstractPersistenceTest implements WithGlobalEntityManager {
+
+  @Test
+  void golondrina() {
+    persist(new Golondrina(10));
+
+    Golondrina pepita = createQuery("from Golondrina", Golondrina.class)
+        .getResultList().get(0);
+
+    assertEquals(10, pepita.getEnergia());
+  }
+}
